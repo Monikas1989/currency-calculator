a92c146c102cd5db1def747d9bb896f4fc8f24fc
Monika Kazula <lorika09022014@gmail.com>
2022-02-27 11:59:16 +0100

    const init added

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
-const sumElement = document.querySelector(".js-sum");
-const currencyElement = document.querySelector(".js-currency");
-const resultElement = document.querySelector(".js-result");
+
 const formElement = document.querySelector(".js-form");
 
 formElement.addEventListener("submit", (event) => {
     event.preventDefault();
-
+    const sumElement = document.querySelector(".js-sum");
+    const currencyElement = document.querySelector(".js-currency");
+    const resultElement = document.querySelector(".js-result");
 
     const sum = sumElement.value;
     const currency = currencyElement.value;
 
-    const result = sum * currency;
+    const result= sum * currency;
 
 
     resultElement.innerText = result;
 });
+    const init = () => {
+        formElement.addEventListener("submit", onFormSubmit);
+            
+    };
+    init();
 
